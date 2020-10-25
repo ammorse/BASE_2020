07a88884e37b6d779e73149dcbaa60157725713e
fabiomarroni <fabio.marroni@gmail.com>
2020-10-25 11:03:09 -0400

    FM added test for 1 environment simulations

--- simulation/programs/command.sh
+++ simulation/programs/command.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sbatch --array=1-2 simulate_and_stan_model_data_analysis_02fm.sbatch 2 design_H1_not_null_H2_not_null_H3_null_01kos.csv design_stan_model_g1_qmodel_and_qsim_same_01kos.csv
+sbatch --array=1-2 simulate_and_stan_model_data_analysis_02fm.sbatch 2 design_H1_not_null_01fm.csv design_stan_model_1env_01fm.csv
