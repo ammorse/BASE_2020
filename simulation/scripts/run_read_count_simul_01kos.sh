30ee734f07df19e0978666c57414b7f92b3cbbba
ksherbina <sherbina@usc.edu>
2020-10-30 11:28:42 -0700

    small changes to some scripts

--- simulation/scripts/run_read_count_simul_01kos.sh
+++ simulation/scripts/run_read_count_simul_01kos.sh
@@ -6,4 +6,3 @@
 
 sbatch simulate_read_counts_NBmodel_01kos.sbatch design_H1_null_01kos.csv 1
 sbatch simulate_read_counts_NBmodel_01kos.sbatch design_H1_null_01kos.csv 2
-sbatch simulate_read_counts_NBmodel_01kos.sbatch design_H1_not-null_01kos.csv 1
